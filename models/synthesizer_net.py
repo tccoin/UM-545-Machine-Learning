d8c1726679a0f157d2bf613ebbff2ef5ddb47115
Jiyi <jiyichen@umich.edu>
2021-12-07 21:21:20 -0500

    update synthesizer net

--- models/synthesizer_net.py
+++ models/synthesizer_net.py
@@ -6,7 +6,7 @@ class SynthesizerNet(nn.Module):
     def __init__(self, fc_dim, activation):
         # Inputs:
         # fc_dim = K (in paper)
-        # activation: choose from {torch.sigmoid, F.relu, F.tanh}
+        # activation: torch.sigmoid, F.relu or F.tanh
         
         super(SynthesizerNet, self).__init__()
         self.scale = nn.Parameter(torch.ones(fc_dim))
