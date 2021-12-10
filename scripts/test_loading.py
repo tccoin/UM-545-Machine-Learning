55e9453a789f4d9dd7e336f517dd2166437ee280
Link Wu <1774241372@qq.com>
2021-12-10 00:21:25 -0500

    detect and fix corrupted frames

--- scripts/test_loading.py
+++ scripts/test_loading.py
@@ -1,9 +1,11 @@
 import json
 import os
+import shutil
 import sys
 import librosa
 import warnings
 warnings.filterwarnings('ignore')
+from PIL import Image, UnidentifiedImageError
 
 class VideoSpliter():
     """split video into frames and audio files """
@@ -21,6 +23,24 @@ class VideoSpliter():
                     audio_raw, rate = librosa.load(audio_path, sr=None, mono=True)
                     assert(audio_raw.shape[0]>0)
                     print('PASS '+audio_path)
+        for instrument in dataset.keys():
+            for i, video_id in enumerate(dataset[instrument]):
+                j = 0
+                while True:
+                    frames_path = os.path.join(frames_dir, instrument, video_id+'.mp4')
+                    frame_path = os.path.join(frames_path, '{}.jpg'.format(j))
+                    if not os.path.exists(frame_path):
+                        break
+                    try:
+                        Image.open(frame_path).convert('RGB')
+                    except UnidentifiedImageError:
+                        print('ERROR: ', frame_path)
+                        os.remove(frame_path)
+                        shutil.copyfile(os.path.join(frames_path, '{}.jpg'.format(j-1)), frame_path)
+                        print('FIX: ', frame_path)
+                    j += 1
+                print('PASS '+ frames_path)
+        
 
 
 if __name__ == '__main__':
